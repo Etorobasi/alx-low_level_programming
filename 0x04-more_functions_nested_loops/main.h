0342e6dba1e81692fbe0e8ac3399a27076b4711c
Etorobasi <etorokpongete@gmail.com>
2023-09-14 16:18:59 -0700

    digits

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 /**
- * int _isupper - Checks for uppercase letters
+ * _isupper - Checks for uppercase letters
  * @c: letter to be checked
+ * _isdigit - checks for digits between 0 and 9
+ * @c: digit to be checked
  */
 int _isupper(int c);
+int _isdigit(int c);
 #endif
