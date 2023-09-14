81db09b93056754633f2b8e0c73708a9b846c269
Etorobasi <etorokpongete@gmail.com>
2023-09-13 19:03:43 -0700

    lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,9 +4,11 @@
  * _putchar - prints characters
  * print_alphabet - prints alphabets
  * @c: character to be printed
+ * print_alphabetx10 - prints alphabets 10 times
  * Return: on success 1
  */
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 #endif
