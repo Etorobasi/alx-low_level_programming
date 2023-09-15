ebadb7da502fa6a5d7e4daaf40f90a79a9a1af4d
Etorobasi <etorokpongete@gmail.com>
2023-09-14 17:28:33 -0700

    multiples

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,13 @@
  * @c: letter to be checked
  * _isdigit - checks for digits between 0 and 9
  * @c: digit to be checked
+ * mul - multiplies 2 integers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: returns a value of 1 is checked successfully, otherwise 0
  */
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
