66b2660215e59c44a50417bd65c7e007c6a29d51
Etorobasi <etorokpongete@gmail.com>
2023-09-14 18:10:10 -0700

    print numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 /**
+ * _putchar - writes the character c to stdout
+ * @c: character to be written to output
  * _isupper - Checks for uppercase letters
  * @c: letter to be checked
  * _isdigit - checks for digits between 0 and 9
@@ -8,10 +10,13 @@
  * mul - multiplies 2 integers
  * @a: integer 1
  * @b: integer 2
+ * print_numbers - print numbers 0-9
  *
  * Return: returns a value of 1 is checked successfully, otherwise 0
  */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 #endif
