c8a32475166083ec5816781f260c42f201ffcfa8
Etorobasi <etorokpongete@gmail.com>
2023-09-15 10:13:29 -0700

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,20 +9,23 @@ void print_diagonal(int n)
 	int c;
 	int b;
 
-	b = 0;
-	while (n > 0)
+	if (n > 0)
 	{
-		c = b;
-		while (c > 0)
+		b = 0;
+		while (n > 0)
 		{
-			_putchar(' ');
-			c--;
+			c = b;
+			while (c > 0)
+			{
+				_putchar(' ');
+				c--;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			b++;
+			n--;
 		}
-		_putchar('\\');
-		_putchar('\n');
-		b++;
-		n--;
 	}
-	if (n <= 0)
-		_putchar('\n');
+	else if (n <= 0)
+	_putchar('\n');
 }
