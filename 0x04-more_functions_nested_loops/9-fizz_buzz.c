c1e43f62fc2b53e08060f420ad27221fd600d95f
Etorobasi <etorokpongete@gmail.com>
2023-09-15 21:51:51 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,23 +13,23 @@ int main(void)
 		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz");
+			printf(" ");
 		}
 		else if (n % 5 == 0)
 		{
 			printf("Buzz");
+			printf(" ");
 		}
 		else if (n % 3 == 0)
 		{
 			printf("Fizz");
+			printf(" ");
 		}
 		else
 		{
 			printf("%d", n);
+			printf(" ");
 		}
-	if (n != 0)
-	{
-		printf(" ");
-	}
 	}
 	printf("\n");
 
