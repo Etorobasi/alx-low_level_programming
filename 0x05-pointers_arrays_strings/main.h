ff9a035bb015a2342b561f80db67d3ba95a7d506
Etorobasi <etorokpongete@gmail.com>
2023-09-19 14:56:19 -0700

    98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar.c - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
