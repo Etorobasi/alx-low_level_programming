d7792fc29777bad3aa087b4064cdae29741949bb
Etorobasi <etorokpongete@gmail.com>
2023-09-20 07:45:10 -0700

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half -  prints half of a string
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int length = 0, i;
+	int n;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		n = length / 2;
+		for (i = 0; i < length; i++)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else if (length % 2 != 0)
+	{
+		n = (length - 1) / 2;
+		for (i = 0; i < length; i++)
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
