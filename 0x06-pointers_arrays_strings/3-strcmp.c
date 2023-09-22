6b5c6ea514ec91322829c4f821d8f124c179e615
Etorobasi <etorokpongete@gmail.com>
2023-09-22 15:33:01 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,15 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int result = 0;
 
-	while (s1[i] == s2[i])
+	for (; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] != '\0' || s2[i] != '\0')
-		break;
-		i++;
+		if (s1[i] != s2[i])
+		{
+			result = s1[i] - s2[i];
+			break;
+		}
 	}
-	return (s1[i] - s2[i]);
+	return (result);
 }
