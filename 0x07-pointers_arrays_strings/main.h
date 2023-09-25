4880e08b0d26bfeb5c3ca4dbc0d27ad7665a4b72
Etorobasi <etorokpongete@gmail.com>
2023-09-25 05:17:20 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1
+ */
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+#endif
