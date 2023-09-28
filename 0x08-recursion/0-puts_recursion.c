a24fec358b7a4899a2762ddb67a265e362b6a412
Etorobasi <etorokpongete@gmail.com>
2023-09-28 03:16:36 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - prints a character to stdout
+ * @c: character to be printed
+ *
+ * Return: returns 1 on success
+ */
+int _putchar(char c);
+void _puts_recursion(char *s);
+#endif
