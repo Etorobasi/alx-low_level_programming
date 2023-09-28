5e6167d18c138d2b3174ceebcea722ff42b9b2ca
Etorobasi <etorokpongete@gmail.com>
2023-09-28 04:26:53 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
 	if (*s == 0 && *s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
-	if (*s != '\0')
-	{
-		_puts_recursion(s);
-	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
