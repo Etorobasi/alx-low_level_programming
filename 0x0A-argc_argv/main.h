aa56f277be21c7f7b9129018c63adef2743813e5
Etorobasi <etorokpongete@gmail.com>
2023-10-02 17:32:25 -0700

    main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to written
+ *
+ * Return: returns 1 on success
+ */
+int _putchar(char c);
+#endif
