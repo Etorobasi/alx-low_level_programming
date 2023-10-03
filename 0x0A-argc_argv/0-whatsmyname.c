15d14104efca1259c1041c47fd94429336799f4f
Etorobasi <etorokpongete@gmail.com>
2023-10-02 17:26:06 -0700

    that is my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,13 +12,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	while (i < 1)
+	for (i = 0; i < argc; i++)
 	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-		i++;
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
