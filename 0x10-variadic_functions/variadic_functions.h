32fe491dd41cbeb1f1ade9b59969d42c01650e6a
Etorobasi <etorokpongete@gmail.com>
2023-10-12 16:36:13 -0700

    vaiadic

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+/**
+ * _putchar - writes the character c to stdou
+ * @c: character to be printed
+ *
+ * Return: 1 on success
+ */
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+#endif
