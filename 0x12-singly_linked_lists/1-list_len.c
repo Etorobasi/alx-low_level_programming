f04afbc0e5a6babe730285ed96c98e0676c0eb38
Etorobasi <etorokpongete@gmail.com>
2023-10-19 21:39:42 -0700

    number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - prints the number of elements in a linked list
+ * @h: pointer to elements in a linked list
+ *
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	unsigned int i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 0; h->next != NULL; i++)
+		h = h->next;
+	return (i);
+}
