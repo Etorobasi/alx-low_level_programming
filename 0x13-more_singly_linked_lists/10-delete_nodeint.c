feb902bf0445e4bc3210d5f26b08b0744aeda655
Etorobasi <etorokpongete@gmail.com>
2023-10-25 18:37:52 -0700

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,8 +17,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	temp = *head;
 	if (head == NULL || *head == NULL)
 		return (-1);
-	if (index == 0)
-		return (-1);
 	for (i = 0; i < index - 1 && temp != NULL && index != 0; i++)
 		temp = temp->next;
 	if (temp == NULL)
