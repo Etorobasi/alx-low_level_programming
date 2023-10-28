6d9f5b3de169ab0bb0ef68de598e8b2ae1427306
Etorobasi <etorokpongete@gmail.com>
2023-10-27 18:48:44 -0700

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,12 +9,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int mask = 1;
+	unsigned int mask;
 
+	mask = 1;
 	mask = mask << index;
-	if ((index > sizeof(unsigned int) * 8 || n == NULL))
+	if (index > sizeof(unsigned long int) * 8 || n == NULL)
 		return (-1);
-	if (((*n >> index) && 1) == 1)
+	if (((*n >> index) & 1) == 1)
 		*n = mask ^ *n;
 	return (1);
 }
