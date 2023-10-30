75b00bba4ce3184b0c0f55e589fcf004e054074c
Etorobasi <etorokpongete@gmail.com>
2023-10-30 14:42:46 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - creates a files
+ * @filename: name of the file to be created
+ * @text_content: NULL terminated string t be written to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i, op;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i]; i++)
+			;
+	}
+	else
+		i = 0;
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	write(op, text_content, i);
+	if (op == -1)
+		return (-1);
+	close(op);
+	return (i);
+}
