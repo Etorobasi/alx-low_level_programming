706b0ddaa403ef49ba818ee3aeaf42760a9747b0
Etorobasi <etorokpongete@gmail.com>
2023-10-30 23:07:26 -0700

    copy file_from file_to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,9 +34,10 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
+
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error(file_from, file_to, argv);
