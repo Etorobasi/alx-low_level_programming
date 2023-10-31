a47ff9f2a394f0bc714102e9a9bf6f9d14443b5f
Etorobasi <etorokpongete@gmail.com>
2023-10-30 22:52:33 -0700

    copy file_from file_to

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * error - handles all the errors
+ * @file_to: file to be copied to
+ * @file_from: file to copy from
+ * @argv: argument vector
+ */
+void error(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[3]);
+		exit(99);
+	}
+}
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1, if successful
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchar, nwr;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error(file_from, file_to, argv);
+
+	nchar = 1024;
+	while (nchar == 1024)
+	{
+		nchar = read(file_from, buf, 1024);
+		if (nchar == -1)
+			error(-1, 0, argv);
+		nwr = write(file_to, buf, 1024);
+		if (nwr == -1)
+			error(0, -1, argv);
+	}
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+	return (0);
+}
